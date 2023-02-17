23295bc71b1c0bab06c5ba2297086bc77f98a497
victor <victor@ieselcaminas.org>
2023-02-17 16:54:37 +0100

    Initial commit

---
+++ src/main/java/org/ieselcaminas/tetris/Shape.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.ieselcaminas.tetris;
+
+/**
+ *
+ * @author victor
+ */
+public class Shape {
+
+    private static final int[][][] COORDS_TABLE = new int[][][]{
+        {{0, 0}, {0, 0}, {0, 0}, {0, 0}},
+        {{0, -1}, {0, 0}, {-1, 0}, {-1, 1}},
+        {{0, -1}, {0, 0}, {1, 0}, {1, 1}},
+        {{0, -1}, {0, 0}, {0, 1}, {0, 2}},
+        {{-1, 0}, {0, 0}, {1, 0}, {0, 1}},
+        {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
+        {{-1, -1}, {0, -1}, {0, 0}, {0, 1}},
+        {{1, -1}, {0, -1}, {0, 0}, {0, 1}}
+    };
+    
+    private Tetrominoes pieceShape;
+    private int coords[][]; 
+    
+    public Shape() {
+        setShape(Tetrominoes.NoShape);
+    }
+    
+    public void setShape(Tetrominoes shapeType) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                coords[i][j] = COORDS_TABLE[shapeType.ordinal()][i][j];
+            }
+        }
+        pieceShape = shapeType;
+    }
+}
---
+++ src/main/java/org/ieselcaminas/tetris/Tetrominoes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package org.ieselcaminas.tetris;
+
+/**
+ *
+ * @author victor
+ */
+public enum Tetrominoes {
+    NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredLShape
+}
